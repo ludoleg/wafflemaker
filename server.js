8ac1e6edcb772a2b3e00f73293fc8f59d74a19f4
ludoleg <ludo@ludovics-imac.attlocal.net>
2015-10-12 16:54:54 -0700

    Changed to dynamic port allocation

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express    =    require('express');
 var app        =    express();
 
+app.set('port', (process.env.PORT || 3000));
+
 require('./router/main')(app);
+
+//views for template files
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server     =    app.listen(3000,function(){
-    console.log("We have started our server on port 3000");
+var server     =    app.listen(app.get('port'), function() {
+    console.log("Waffle app is running");
 });
 
